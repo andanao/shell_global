d0bdd2f2bf82553aba92c594d1d8067745fda1bb
Adrian Danao-Schroeder <adrian.danao.schroeder@ses.com>
2023-06-26 17:07:13 -0400

    write to bashrc for setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+if [ -n "$shell_global_setup"]; then
+    # Uppercase filenames are cancer
+    echo "bind 'set completion-ignore-case on'" >> .bashrc
+    echo "shell_global_setup=1" >> .bashrc
+fi
+
+
+
 scriptdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
