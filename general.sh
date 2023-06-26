8e1fd63741b0ebee1e964e9b475ac731f2facecd
Adrian Danao-Schroeder <adrian.danao.schroeder@ses.com>
2023-06-26 17:07:02 -0400

    remove bind

--- general.sh
+++ general.sh
@@ -1,6 +1,3 @@
-# Uppercase filenames are cancer
-bind 'set completion-ignore-case on'
-
 scriptdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 sh $scriptdir/aliases.sh
 #sh $scriptdir/functions.sh
