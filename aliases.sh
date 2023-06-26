40b3bb27f852d8f60ffac972837694df9af5912d
Adrian Danao-Schroeder <adrian.danao.schroeder@ses.com>
2023-06-26 11:57:42 -0400

    initial commit

---
+++ aliases.sh
@@ -0,0 +1,23 @@
+
+
+alias up="cd .."
+alias back="cd -"
+
+alias la="ls -la --color=auto"
+alias ll="ls -la --color=auto"
+
+alias grepr="grep -ri"
+alias grepf="find | grep -i"
+
+
+alias vi="vim"
+alias vir="vim -R"
+
+
+# git
+alias g=git
+alias glog="git log --oneline"
+alias glogs="git log -10 --oneline"
+alias gp="git pull"
+alias gpl="git reset --hard && git pull && git log -1 --oneline" #I'm lazy sometimes
+
