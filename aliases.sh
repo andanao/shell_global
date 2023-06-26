1ea4b5314cc69509c888188316a440ac64b7fc5e
Adrian Danao-Schroeder <adrian.danao.schroeder@ses.com>
2023-06-26 15:08:55 -0400

    comment headers and dush

--- aliases.sh
+++ aliases.sh
@@ -1,23 +1,24 @@
-
-
+# movement
 alias up="cd .."
 alias back="cd -"
 
+# files
 alias la="ls -la --color=auto"
 alias ll="ls -la --color=auto"
 
+# grep
 alias grepr="grep -ri"
 alias grepf="find | grep -i"
 
-
+# vim
 alias vi="vim"
 alias vir="vim -R"
 
-
 # git
-alias g=git
 alias glog="git log --oneline"
 alias glogs="git log -10 --oneline"
 alias gp="git pull"
 alias gpl="git reset --hard && git pull && git log -1 --oneline" #I'm lazy sometimes
 
+# Misc
+alias dush='du -sh -- *'
